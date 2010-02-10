270b6d27a482fae37c319df797958c5ea316a09f
Kousuke Ebihara <ebihara@tejimaya.com>
2010-02-10 15:28:18 +0900

    fixed the default routing url that is broken by extended routing rules

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect 'projects/:id/members.:format', :controller => 'openpne_members', :action => 'new', :format => /xml/
-  map.connect 'users/:username.:format', :controller => 'openpne_users', :action => 'show', :format => /xml/
+  map.connect 'projects/:id/members.xml', :controller => 'openpne_members', :action => 'new', :format => /xml/
+  map.connect 'users/:username.xml', :controller => 'openpne_users', :action => 'show', :format => /xml/
 end
